80ce03ca690a8ce6c394c2d4fadb1c7b1552c8fa
huloves <17791627852@163.com>
2020-01-23 23:33:29 +0800

    alarm

---
+++ Unix-Linux/alarm/ticker_demo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <signal.h>
+#include "../my_function/set_ticker.h"
+
+int main()
+{
+    void countdown(int);
+
+    signal(SIGALRM, countdown);
+    if(set_ticker(500) == -1)
+        perror("set_ticker");
+    else
+        while(1)
+            pause();
+
+    return 0;
+}
+
+void countdown(int signum)
+{
+    static int num = 10;
+    printf("%d..", num--);
+    fflush(stdout);
+    if(num < 0) {
+        printf("DOWN! \n");
+        exit(0);
+    }
+}
+
+/*int set_ticker(int n_msecs)
+{
+    struct itimerval new_timeset;
+    long n_sec, n_usecs;
+
+    n_sec = n_msecs / 1000;
+    n_usecs = (n_msecs % 1000) * 1000L;
+
+    new_timeset.it_interval.tv_sec = n_sec;
+    new_timeset.it_interval.tv_usec = n_usecs;
+
+    new_timeset.it_value.tv_sec = n_sec;
+    new_timeset.it_value.tv_usec = n_usecs;
+
+    return setitimer(ITIMER_REAL, &new_timeset, NULL);
+}*/
---
+++ my_function/set_ticker.h
@@ -0,0 +1,26 @@
+#ifndef SET_TICKER_H
+#define SET_TICKER_H
+
+#include <unistd.h>
+#include <sys/time.h>
+
+int set_ticker(int n_msecs);
+
+int set_ticker(int n_msecs)
+{
+    struct itimerval new_timeset;
+    long n_sec, n_usecs;
+
+    n_sec = n_msecs / 1000;
+    n_usecs = (n_msecs % 1000) * 1000L;
+
+    new_timeset.it_interval.tv_sec = n_sec;
+    new_timeset.it_interval.tv_usec = n_usecs;
+
+    new_timeset.it_value.tv_sec = n_sec;
+    new_timeset.it_value.tv_usec = n_usecs;
+
+    return setitimer(ITIMER_REAL, &new_timeset, NULL);
+}
+
+#endif
