d4804dac0a4f7aa7cfdc3a07ccb152eb98cf4c4f
huloves <17791627852@163.com>
2020-02-06 01:59:04 +0800

    用户程序重定位完成

--- x86-code/c16/c16_core.asm
+++ x86-code/c16/c16_core.asm
@@ -298,6 +298,88 @@ make_seg_descriptor:                        ;构造存储器和系统的段描述符
          retf
 
 ;------------------------------------------------------------------------
+allocate_a_4k_page:							;分配一个4KB的页
+											;输入：无
+											;输出：EAX=页的物理地址
+	push ebx
+	push ecx
+	push edx
+	push ds
+
+	mov eax,core_data_seg_sel
+	mov ds,eax
+
+	xor eax,eax
+.b1:
+	bts [page_bit_map],eax
+	jnc .b2
+	inc eax
+	cmp eax,page_map_len*8
+	jl .b1
+
+	mov ebx,message_3
+	call sys_routine_seg_sel:put_string
+	hlt										;没有可以分配的页，停机
+
+.b2:
+	shl eax,12								;乘以0x1000(4096)
+
+	pop ds
+	pop edx
+	pop ecx
+	pop ebx
+
+	ret
+
+;------------------------------------------------------------------------
+alloc_inst_a_page:							;分配一个页，并安装在当前活动的
+											;层级分页结构中
+											;输入：EBX=页的线性地址
+	push eax
+	push ebx
+	push esi
+	push ds
+
+	mov eax,mem_0_4_gb_seg_sel
+	mov ds,eax
+
+	;检查该线性地址所对应的页是否存在
+	mov esi,ebx
+	and esi,0xffc00000
+	shr esi,20								;得到页目录索引，并乘以4
+	or esi,0xfffff000						;页目录自身的线性地址+表内偏移
+
+	test dword [esi],0x00000001
+	jnz .b1
+
+	;创建该线性地址所对应的页表
+	call allocate_a_4k_page
+	or eax,0x00000007
+	mov [esi],eax							;在页目录中登记新分配的页表
+
+.b1:
+	;开始访问该线性地址所对应的页表
+	mov esi,ebx
+	shr esi,10
+	and esi,0x003ff000
+	or esi,0xffc00000
+
+	;得到该线性地址在页表内的对应条目（页表项）
+	and ebx,0x003ff000
+	shr ebx,10								;相当于右移12位，再乘以4
+	or esi,ebx								;页表项的线性地址
+	call allocate_a_4k_page					;分配一个页，要安装的页
+	or eax,0x00000007
+	mov [esi],eax
+
+	pop ds
+	pop esi
+	pop ebx
+	pop eax
+
+	retf
+
+;------------------------------------------------------------------------
 make_gate_descriptor:                       ;构造门的描述符（调用门等）
                                             ;输入：EAX=门代码在段内偏移地址
                                             ;       BX=门代码所在段的选择子 
@@ -416,6 +498,301 @@ core_data_end:
 
 ;========================================================================
 SECTION core_code vstart=0
+;---------------------------------------------------------------------
+load_relocate_program:						;加载并重定位用户程序
+											;输入：PUSH 逻辑扇区号
+											;	   push 任务控制块基地址
+											;输出：无
+	pushad
+
+	push ds
+	push es
+
+	mov ebp,esp
+
+	mov ecx,mem_0_4_gb_seg_sel
+	mov es,ecx
+
+	;清空当前页目录的前半部分（对应低2GB的局部地址空间）
+	mov ebx,0xfffff000
+	xor esi,esi
+.b1:
+	mov dword [es:ebx+esi*4],0x00000000
+	inc esi
+	cmp esi,512
+	jl .b1
+
+	;开始分配内存并加载用户程序
+	mov eax,core_data_seg_sel
+	mov ds,eax								;切换到内核数据段
+
+	mov eax,[ebp+12*4]						;从堆栈中读取用户程序起始扇区号
+	mov ebx,core_buf						;读取程序头部段
+	call sys_routine_seg_sel:read_hard_disk_0
+
+	;判断用户程序大小
+	mov eax,[core_buf]
+	mov ebx,eax
+	and ebx,0xfffff000
+	add ebx,0x1000
+	test eax,0x00000fff
+	cmovnz eax,ebx
+
+	mov ecx,eax
+	shr ecx,12								;程序占用的总4KB页数
+
+	mov eax,[ebp+12*4]						;读取起始逻辑扇区号
+	mov esi,[ebp+11+4]						;读取TCB基地址
+.b2:
+	mov ebx,[es:esi+0x06]					;读取可用的线性地址
+	add dword [es:esi+0x06],0x1000
+	call sys_routine_seg_sel:alloc_inst_a_page
+
+	push ecx
+	mov ecx,8
+.b3:
+	call sys_routine_seg_sel:read_hard_disk_0
+	inc eax
+	loop .b3
+
+	pop ecx
+	loop .b2
+
+	;在内核地址空间内创建用户任务的TSS
+	mov eax,core_data_seg_sel
+	mov ds,eax
+
+	mov ebx,[core_next_laddr]
+	call sys_routine_seg_sel:alloc_inst_a_page
+	add dword [core_next_laddr],4096
+
+	mov [es:esi+0x14],ebx					;在TCB中填写TSS的线性地址
+	mov word [es:esi+0x12],103				;在TCB中填写TSS的界限值
+
+	;在用户任务的局部地址空间内创建LDT
+	mov ebx,[es:esi+0x06]					;在TCB中读取可用的线性地址
+	add dword [es:esi+0x06],0x1000
+	call sys_routine_seg_sel:alloc_inst_a_page
+	mov [es:esi+0x0c],ebx					;在TCB中填写LDT的线性地址
+
+	;建立程序的代码段描述符
+	mov eax,0x00000000
+	mov ebx,0x000fffff
+	mov ecx,0x00c0f800						;4KB粒度的代码段描述符，特权级3
+	call sys_routine_seg_sel:make_seg_sel_descriptor
+	mov ebx,esi
+	call fill_descriptor_in_ldt
+	or cx,0000_0000_0000_0011B				;设置选择子的特权级为3
+
+	mov ebx,[es:esi+0x14]					;从TCB中获取TSS的线性地址
+	mov [es:ebx+76],cx						;填写TSS的CS域
+
+	;建立程序数据段描述符
+	mov eax,0x00000000
+	mov ebx,0x000fffff
+	mov ecx,0x00c0f200						;4KB粒度的数据段描述符，特权级3
+	call sys_routine_seg_sel:make_seg_descriptor
+	mov ebx,esi
+	call fill_descriptor_in_ldt
+	or cx,0000_0000_0000_0011B				;设置选择子的特权级为3
+
+	mov ebx,[es:esi+0x14]					;从TCB中获取TSS的线性地址
+	mov [es:ebx+84],cx						;DS域
+	mov [es:ebx+72],cx						;ES域
+	mov [es:ebx+88],cx						;FS域
+	mov [es:ebx+92],cx						;GS域
+
+	;将数据段作为用户任务的3特权级固有堆栈
+	mov ebx,[es:esi+0x06]					;从TCB中获取可用的线性地址
+	add dword [es:esi+0x06],0x1000
+	call sys_routine_seg_sel:alloc_inst_a_page
+
+	mov ebx,[es:esi+0x14]					;从TCB获取TSS的线性地址
+	mov [es:ebx+80],cx						;填写SS域
+	mov edx,[es:esi+0x06]					;堆栈的高端线性地址
+	mov [es:ebx+56],edx						;填写TSS的ESP域
+
+	;在用户任务的局部地址空间内创建0特权级堆栈
+	mov ebx,[es:esi+0x06]
+	add [es:esi+0x06],0x1000
+	call sys_routine_seg_sel:alloc_inst_a_page
+
+	mov eax,0x00000000
+	mov ebx,0x000fffff
+	mov ecx,0x00c09200						;4KB粒度的堆栈段描述符，特权级0
+	call sys_routine_seg_sel:make_descriptor
+	mov ebx,esi
+	call fill_descriptor_in_ldt
+	or cx,0000_0000_0000_0000B				;设置选择子的特权级为0
+
+	mov ebx,[es:esi+0x14]					;从TCB中获取TSS的线性地址
+	mov [es:esi+8],cx						;填写TSS的SS0域
+	mov edx,[es:esi+0x06]					;堆栈的高端线性地址
+	mov [es:ebx+4],edx						;填写TSS的ESP0域
+
+	;在用户任务的局部空间内创建1特权级堆栈
+	mov ebx,[es:esi+0x06]
+	add [es:esi+0x06],0x1000
+	call sys_routine_seg_sel:alloc_inst_a_page
+
+	mov eax,0x00000000
+	mov ebx,0x000fffff
+	mov ecx,0x00c0b200						;4KB粒度的堆栈段描述符，特权级1
+	call sys_routine_seg_sel:make_seg_descriptor
+	mov ebx,esi
+	call fill_descriptor_in_ldt
+	or cx,0000_0000_0000_0001B				;设置选择子的特权级为1
+
+	mov ebx,[es:esi+0x14]
+	mov [es:ebx+16],cx
+	mov edx,[es:esi+0x06]
+	mov [es:ebx+12],edx
+
+	;在用户任务的局部地址空间内创建2特权级堆栈
+	mov ebx,[es:esi+0x06]
+	add [es:esi+0x06],0x1000
+	call sys_routine_seg_sel:alloc_inst_a_page
+
+	mov eax,0x00000000
+	mov ebx,0x000fffff
+	mov ecx,0x00c0d200
+	call sys_routine_seg_sel:make_seg_descriptor
+	mov ebx,esi
+	call fill_descriptor_in_ldt
+	or cx,0000_0000_0010B
+
+	mov ebx,[es:esi+0x14]
+	mov [es:ebx+24],cx
+	mov edx,[es:esi+0x06]
+	mov [es:esi+20],edx
+
+	;重定位SALT
+	mov eax,mem_0_4_gb_seg_sel
+	mov es,eax
+
+	mov eax,core_data_seg_sel
+	mov ds,eax
+
+	cld
+
+	mov ecx,[es:0x0c]						;U-SALT条目数
+	mov edi,[es:0x08]						;U-SALT在4GB空间内的偏移
+.b4:
+	push ecx
+	push edi
+
+	mov ecx,salt_items
+	mov esi,salt
+.b5:
+	push edi
+	push esi
+	push ecx
+
+	mov ecx,64
+	repe cmpsd
+	jnz .b6
+	mov eax,[esi]
+	mov [es:edi-256],eax
+	mov ax,[esi+4]
+	or ax,0000000000000011B
+
+	mov [es:edi-252],ax
+.b6:
+	pop ecx
+	pop esi
+	add esi,salt_item_len
+	pop edi
+	loop .b5
+
+	pop edi
+	add edi,256
+	pop ecx
+	loop .b4
+
+	;在GDT中登记LDT描述符
+	mov esi,[ebp+11*4]						;从堆栈中取得TCB的基地址
+	mov eax,[es:esi+0x0c]					;LDT的起始线性地址
+	movzx ebx,word [es:esi+0x0a]			;LDT断界限
+	mov ecx,0x00408200						;属性，LDT描述符，特权级0
+	call sys_routine_seg_sel:make_seg_descriptor
+	call sys_routine_seg_sel:set_up_gdt_descriptor
+	mov [es:esi+0x10],cx					;登记LDT选择子到TCB中
+
+	mov word [es:ebx+0],0					;反向链=0
+
+	mov dx,[es:esi+0x12]					;段长度（界限）
+	mov [es:ebx+102],dx						;填写TSS的I/O位图偏移域
+
+	mov word [es:ebx+100],0					;T=0
+
+	mov eax,[es:0x04]						;用户程序的入口点
+	mov [es:ebx+32],eax						;填写TSS的EIP域
+
+	pushfd
+	pop edx
+	mov [es:ebx+36],edx						;填写TSS的EFLAGS域
+
+	;在GDT中登记TSS描述符
+	mov eax,[es:esi+0x14]
+	movzx ebx,word [es:esi+0x12]
+	mov ecx,0x00408900						;TSS描述符，特权级0
+	call sys_routine_seg_sel:make_seg_descriptor
+	call sys_routine_seg_sel:set_up_gdt_descriptor
+	mov [es:esi+0x18],cx					;登记TSS选择子到TCB
+
+	;创建用户任务的页目录
+	;注意！页的分配和使用是由位图决定的，可以不占用线性地址空间
+	call sys_routine_seg_sel:create_copy_cur_pdir
+	mov ebx,[es:esi+0x14]					;从TCB中获取TSS的线性地址
+	mov dword [es:ebx+28],eax				;填写TSS的CR3（PDBR）域
+
+	pop es
+	pop ds
+
+	popad
+
+	ret 8
+
+;---------------------------------------------------------------------
+append_to_tcb_link:                         ;在TCB链上追加任务控制块
+                                            ;输入：ECX=TCB线性基地址
+         push eax
+         push edx
+         push ds
+         push es
+         
+         mov eax,core_data_seg_sel          ;令DS指向内核数据段 
+         mov ds,eax
+         mov eax,mem_0_4_gb_seg_sel         ;令ES指向0..4GB段
+         mov es,eax
+         
+         mov dword [es: ecx+0x00],0         ;当前TCB指针域清零，以指示这是最
+                                            ;后一个TCB
+                                             
+         mov eax,[tcb_chain]                ;TCB表头指针
+         or eax,eax                         ;链表为空？
+         jz .notcb 
+         
+  .searc:
+         mov edx,eax
+         mov eax,[es: edx+0x00]
+         or eax,eax               
+         jnz .searc
+         
+         mov [es: edx+0x00],ecx
+         jmp .retpc
+         
+  .notcb:       
+         mov [tcb_chain],ecx                ;若为空表，直接令表头指针指向TCB
+         
+  .retpc:
+         pop es
+         pop ds
+         pop edx
+         pop eax
+         
+         ret
+
 ;------------------------------------------------------------------------
 start:
 	mov ecx,core_data_seg_sel
@@ -559,3 +936,62 @@ flush:
 	call far [salt_1+256]				;通过门显示信息（偏移量将被忽略）
 
 	;为程序管理器的TSS分配内存空间
+	mov ebx,[core_next_laddr]
+	call sys_routine_seg_sel:alloc_inst_a_page
+	add dword [core_next_laddr],4096
+
+	;在程序管理器的TSS中设置必要的项目
+	mov word [es:ebx],0					;反向链=0
+
+	mov eax,cr3
+	mov dword [es:ebx+28],eax			;登记cr3（PDBR）
+
+	mov word [es:ebx+96],0
+	mov wore [es:ebx+100],0
+	mov word [es:ebx+102],103
+
+	;创建任务管理器的TSS描述符，并安装到GDT
+	mov eax,ebx
+	mov ebx,103
+	mov ecx,0x00408900
+	call sys_routine_seg_sel:make_seg_descriptor
+	call sys_routine_seg_sel:set_up_gdt_descriptor
+	mov [program_man_tss+4],cx			;保存任务管理器的TSS选择子
+
+	;任务寄存器TR中的内容是任务存在的标志，该内容也决定了当前任务是谁。
+	;下面的指令为当前正在执行的0特权级任务“程序管理器”后补手续（TSS）。
+	ltr cx
+
+	;任务管理器 正在执行中
+
+	;创建用户任务的任务控制块
+	mov ebx,[core_next_laddr]
+	call sys_routine_seg_sel:alloc_inst_a_page
+	add dword [core_next_laddr],4096
+
+	mov dword [es:ebx+0x06],0			;用户任务局部空间的分配从0开始
+	mov word [es:ebx+0x0a],0xffff
+	mov ecx,ebx
+	call append_to_tcb_link
+
+	push dword 50
+	push ecx
+
+	call load_relacate_program
+
+	mov ebx,message_4
+	call sys_routine_seg_sel:put_string
+
+	call far [es:ecx+0x14]
+
+	mov ebx,message_5
+	call sys_routine_seg_sel
+
+	hlt
+
+core_code_end:
+
+;---------------------------------------------------------------------
+SECTION core_trail
+;---------------------------------------------------------------------
+core_end:
