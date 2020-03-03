6b08736d3a3e9ff442ca1bc83a51872f48916629
huloves <17791627852@163.com>
2020-03-03 23:21:45 +0800

    inline asm study

---
+++ os-truth/c/c06/practice/mem.s
@@ -0,0 +1,86 @@
+	.file	"mem.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"in_b is %d\n"
+.LC1:
+	.string	"in_b now is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ebx
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x78,0x6
+	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.bx
+	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
+	movl	%gs:20, %eax
+	movl	%eax, -12(%ebp)
+	xorl	%eax, %eax
+	movl	$1, -16(%ebp)
+	movl	$2, -20(%ebp)
+	movl	-20(%ebp), %eax
+	subl	$8, %esp
+	pushl	%eax
+	leal	.LC0@GOTOFF(%ebx), %eax
+	pushl	%eax
+	call	printf@PLT
+	addl	$16, %esp
+	movl	-16(%ebp), %eax
+#APP
+# 8 "mem.c" 1
+	movb %al, -20(%ebp)
+# 0 "" 2
+#NO_APP
+	movl	-20(%ebp), %eax
+	subl	$8, %esp
+	pushl	%eax
+	leal	.LC1@GOTOFF(%ebx), %eax
+	pushl	%eax
+	call	printf@PLT
+	addl	$16, %esp
+	nop
+	movl	-12(%ebp), %eax
+	xorl	%gs:20, %eax
+	je	.L2
+	call	__stack_chk_fail_local
+.L2:
+	leal	-8(%ebp), %esp
+	popl	%ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	popl	%ebx
+	.cfi_restore 3
+	popl	%ebp
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.bx,"axG",@progbits,__x86.get_pc_thunk.bx,comdat
+	.globl	__x86.get_pc_thunk.bx
+	.hidden	__x86.get_pc_thunk.bx
+	.type	__x86.get_pc_thunk.bx, @function
+__x86.get_pc_thunk.bx:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %ebx
+	ret
+	.cfi_endproc
+.LFE1:
+	.hidden	__stack_chk_fail_local
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
