bf6ba1d57a99f6cffc0a30e3a25e72c2b9bfe07a
huloves <17791627852@163.com>
2020-03-13 22:58:40 +0800

    PIT contral setting

---
+++ os-truth/device/timer.c
@@ -0,0 +1,26 @@
+#include "io.h"
+#include "print.h"
+
+#define IRQ0_FREQUENCY      100
+#define INPUT_FREQUENCY     1193180
+#define COUNTER0_VALUE      INPUT_FREQUENCY / IRQ0_FREQUENCY
+#define COUNTER0_PORT       0x40
+#define COUNTER0_NO         0
+#define COUNTER_MODE        2
+#define READ_WRITE_LATCH    3
+#define PIT_CONTROL_PORT    0x43
+
+/*把操作的计数器counter_no、读写锁属性rwl、计数器模式counter_mode写入模式控制寄存器并赋予初始值counter_value*/
+static void frequency_set(uint8_t counter_port, \
+                          uint8_t counter_no, \
+                          uint8_t rwl, \
+                          uint8_t counter_mode, \
+                          uint16_t counter_value)
+{
+    //往控制字寄存器端口0x43中写入控制字
+    outb(PIT_CONTROL_PORT, (uint8_t)(counter_no << 6 | rwl << 4 | counter_mode <<1));
+    //先写入counter_value的低8位
+    outb(counter_port, (uint8_t)counter_value);
+    //在写入counter_value的高8位
+    outb(counter_port, (uint8_t)counter_value >> 8);
+}
