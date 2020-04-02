2c48b45daedd2903cdfe866fdfc6030f609735cf
huloves <17791627852@163.com>
2020-04-03 00:35:07 +0800

    thread of kernel finish

--- os-truth/kernel/main.c
+++ os-truth/kernel/main.c
@@ -3,6 +3,7 @@
 #include "debug.h"
 #include "memory.h"
 #include "thread.h"
+#include "interrupt.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
@@ -15,6 +16,7 @@ int main(void)
     thread_start("k_thread_a", 31, k_thread_a, "argA ");
     thread_start("k_thread_b", 8, k_thread_b, "argB ");
 
+    intr_enable();   //打开中断
     while(1) {
         put_str("Main ");
     }
