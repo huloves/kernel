c4b200b859323997df08ddbd24c0a09cf4b29f83
huloves <17791627852@163.com>
2020-04-09 19:06:20 +0800

    console finish

--- os-truth/kernel/debug.h
+++ os-truth/kernel/debug.h
@@ -6,18 +6,17 @@ void panic_spin(char* filename, int line, const char* func, const char* conditio
  * _VA_ARGS是预处理器所支持的专用标识符
  * 代表所有与省略号相对应的参数
  * "..."表示定义的宏其参数可变
- * **********************
-*/
+ * ***********************/
 #define PANIC(...) panic_spin(__FILE__, __LINE__, __func__, __VA_ARGS__)
 
-#ifndef NDEBUG
+#ifdef NDEBUG
     #define ASSERT(CONDITION) ((void)0)
 #else
-#define ASSERT(CONDITION)\
-if(CONDITION){}else{ \
+    #define ASSERT(CONDITION)\
+        if(CONDITION) {} else { \
     /*符号#让编译器将宏的参数转化为字符串面量*/ \
-    PANIC(#CONDITION); \
-}
+        PANIC(#CONDITION); \
+    }
 #endif /*NDEBUG*/
 
 #endif /*_KERNEL_DEBUG_H*/
