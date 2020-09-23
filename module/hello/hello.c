586918d1f896ccb1092674301b9446bb94db7271
huloves <17791627852@163.com>
2020-09-23 19:58:25 +0800

    change module-hello

--- module/hello/hello.c
+++ module/hello/hello.c
@@ -1,21 +1,41 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <uapi/linux/sched.h>
+#include <linux/init_task.h>
+#include <linux/fdtable.h>
+#include <linux/fs_struct.h>
+#include <linux/mm_types.h>
+#include <linux/string.h>
+#include <linux/signal.h>
+#include <asm/signal.h>
+#include <linux/kallsyms.h>
 
-MODULE_LICENSE("GPL");
-
-static int year=2020;
-
-static int hello_init(void)
+//内核模块初始化函数
+static int __init traverse_pcb(void)
 {
-    printk(KERN_WARNING "hello kernel, it is %d\n",year);
+    struct task_struct* task;
+    struct task_struct* p;
+    struct list_head* pos;
+    int count = 0;
+    
+    task = &init_task;   //指向0号进程的pcb
+
+    list_for_each(pos, &task->tasks) {
+        p = list_entry(pos, struct task_struct, tasks);   //指向遍历的当前进程
+        count++;
+        printk("pid:%d  state:%lx name:%s\n", p->pid, p->state, p->comm);
+    }
+    printk("task number = %d\n", count);
+
     return 0;
 }
 
-static void hello_exit(void)
+static void __exit end_pcb(void)
 {
-    printk("Bye, kernel!\n");
+    printk("traverse pcb is end\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(traverse_pcb);
+module_exit(end_pcb);
+MODULE_LICENSE("GPL");
