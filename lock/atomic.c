62d1a15001840757a901cafd64a13eaab0dacc40
huloves <17791627852@163.com>
2025-01-22 20:12:29 +0800

    update atomic test

--- lock/atomic.c
+++ lock/atomic.c
@@ -3,20 +3,26 @@
 #include <linux/init.h>
 #include <linux/kthread.h>
 #include <linux/atomic.h>
+#include <linux/percpu.h>
 #include <linux/delay.h>
 #include <linux/sched.h>
 #include <linux/compaction.h>
 #include <linux/ktime.h>
 
+#define KTHREAD_NUM	8
+
 static int __init atomic_init(void);
 static void __exit atomic_exit(void);
 
-static volatile unsigned long global_var = 0;
+static volatile u32 global_var = 0;
 static atomic_t atomic_global_var = ATOMIC_INIT(0);
+static volatile u32 spin_global_var = 0;
+static spinlock_t spinlock;
+DEFINE_PER_CPU(u32, percpu_global_var);
 
-static struct task_struct *test_kthread[4] = { NULL, NULL, NULL, NULL };
-static struct completion kthread_completion[4];
-static u64 kthread_runtime_ns[4] = { 0 };
+static struct task_struct *test_kthread[KTHREAD_NUM];
+static struct completion kthread_completion[KTHREAD_NUM];
+static u64 kthread_runtime_ns[KTHREAD_NUM] = { 0 };
 
 static int kthread_ordinary_func(void *arg)
 {
@@ -30,7 +36,7 @@ static int kthread_ordinary_func(void *arg)
 	/* 获取起始时间点 */
 	start = ktime_get();
 
-	for (i = 0; i < 1000000000; i++) {
+	for (i = 0; i < 100000000; i++) {
 		global_var++;
 	}
 
@@ -60,7 +66,7 @@ static int kthread_atomic_func(void *arg)
 	/* 获取起始时间点 */
 	start = ktime_get();
 
-	for (i = 0; i < 1000000000; i++) {
+	for (i = 0; i < 100000000; i++) {
 		atomic_inc(&atomic_global_var);
 	}
 
@@ -78,64 +84,128 @@ static int kthread_atomic_func(void *arg)
 	return 0;
 }
 
+static int kthread_spinlock_func(void *arg)
+{
+	const char *task_name = current->comm;
+	u64 index = (u64)arg;
+	ktime_t start, end;
+	int i;
+
+	printk(KERN_INFO "[%s] start\n", task_name);
+
+	/* 获取起始时间点 */
+	start = ktime_get();
+
+	for (i = 0; i < 100000000; i++) {
+		spin_lock(&spinlock);
+		spin_global_var++;
+		spin_unlock(&spinlock);
+	}
+
+	/* 获取结束时间点 */
+	end = ktime_get();
+
+	if (index == 4) {
+		complete(&kthread_completion[4]);
+		kthread_runtime_ns[4] = ktime_to_ns(ktime_sub(end, start));
+	} else {
+		complete(&kthread_completion[5]);
+		kthread_runtime_ns[5] = ktime_to_ns(ktime_sub(end, start));
+	}
+
+	return 0;
+}
+
+static int kthread_percpu_func(void *arg)
+{
+	const char *task_name = current->comm;
+	u64 index = (u64)arg;
+	ktime_t start, end;
+	int i;
+
+	printk(KERN_INFO "[%s] start\n", task_name);
+
+	/* 获取起始时间点 */
+	start = ktime_get();
+
+	for (i = 0; i < 100000000; i++) {
+		this_cpu_inc(percpu_global_var);
+	}
+
+	/* 获取结束时间点 */
+	end = ktime_get();
+
+	if (index == 6) {
+		complete(&kthread_completion[6]);
+		kthread_runtime_ns[6] = ktime_to_ns(ktime_sub(end, start));
+	} else {
+		complete(&kthread_completion[7]);
+		kthread_runtime_ns[7] = ktime_to_ns(ktime_sub(end, start));
+	}
+
+	return 0;
+}
+
+static int create_and_start_kthread(struct task_struct **thread,
+				   int (*thread_func)(void *arg),
+				   void *arg,
+				   const char *name,
+				   int cpu)
+{
+	*thread = kthread_create(thread_func, arg, name);
+	if (!(*thread) || IS_ERR(*thread)) {
+		printk(KERN_DEBUG "%s()-%d: kthread_create fail\n", __func__, __LINE__);
+		return -ECHILD;
+	}
+	kthread_bind(*thread, cpu);
+	wake_up_process(*thread);
+	return 0;
+}
+
 static int __init atomic_init(void)
 {
+	int i;
+	int result;
+
 	printk(KERN_INFO "install atomic module.\n");
 
-	init_completion(&kthread_completion[0]);
-	init_completion(&kthread_completion[1]);
-	init_completion(&kthread_completion[2]);
-	init_completion(&kthread_completion[3]);
+	for (i = 0; i < KTHREAD_NUM; i++)
+		init_completion(&kthread_completion[i]);
 
-	test_kthread[0] = kthread_create(kthread_ordinary_func, (void *)0, "ordinary-0");
-	if (!test_kthread[0] || IS_ERR(test_kthread[0])) {
-		printk(KERN_DEBUG "%s()-%d: kthread_create fail \n", __func__, __LINE__);
-		return -ECHILD;
-	}
-	kthread_bind(test_kthread[0], 0);
-	wake_up_process(test_kthread[0]);
+	for (i = 0; i < KTHREAD_NUM; i++) {
+		int (*thread_func)(void *) = (i < 2) ? kthread_ordinary_func :
+					     (i < 4) ? kthread_atomic_func :
+					     (i < 6) ? kthread_spinlock_func :
+					     	       kthread_percpu_func;
+		int cpu = i % 2;
+		char task_name[TASK_COMM_LEN];
 
-	test_kthread[1] = kthread_create(kthread_ordinary_func, (void *)1, "ordinary-1");
-	if (!test_kthread[1] || IS_ERR(test_kthread[1])) {
-		printk(KERN_DEBUG "%s()-%d: kthread_create fail \n", __func__, __LINE__);
-		return -ECHILD;
-	}
-	kthread_bind(test_kthread[1], 1);
-	wake_up_process(test_kthread[1]);
+		sprintf(task_name, "kthread-%d(%d)", i, cpu);
 
-	test_kthread[2] = kthread_create(kthread_atomic_func, (void *)2, "atomic-0");
-	if (!test_kthread[2] || IS_ERR(test_kthread[2])) {
-		printk(KERN_DEBUG "%s()-%d: kthread_create fail \n", __func__, __LINE__);
-		return -ECHILD;
+		result = create_and_start_kthread(&test_kthread[i], thread_func, (void *)(long)i, task_name, cpu);
+		if (result)
+			return result;
 	}
-	kthread_bind(test_kthread[2], 0);
-	wake_up_process(test_kthread[2]);
-
-	test_kthread[3] = kthread_create(kthread_atomic_func, (void *)3, "atomic-1");
-	if (!test_kthread[3] || IS_ERR(test_kthread[3])) {
-		printk(KERN_DEBUG "%s()-%d: kthread_create fail \n", __func__, __LINE__);
-		return -ECHILD;
-	}
-	kthread_bind(test_kthread[3], 1);
-	wake_up_process(test_kthread[3]);
 
 	return 0;
 }
 
 static void __exit atomic_exit(void)
 {
-	wait_for_completion(&kthread_completion[0]);
-	wait_for_completion(&kthread_completion[1]);
-	wait_for_completion(&kthread_completion[2]);
-	wait_for_completion(&kthread_completion[3]);
+	int i;
 
-	printk(KERN_INFO "value of global_var: %ld\n", global_var);
-	printk(KERN_INFO "value of atomic_global_var: %d\n", atomic_read(&atomic_global_var));
+	for (i = 0; i < KTHREAD_NUM; i++)
+		wait_for_completion(&kthread_completion[i]);
 
-	printk(KERN_INFO "Thread 0 run time: %llu ns\n", kthread_runtime_ns[0]);
-	printk(KERN_INFO "Thread 1 run time: %llu ns\n", kthread_runtime_ns[1]);
-	printk(KERN_INFO "Thread 2 run time: %llu ns\n", kthread_runtime_ns[2]);
-	printk(KERN_INFO "Thread 3 run time: %llu ns\n", kthread_runtime_ns[3]);
+	printk(KERN_INFO "global_var: %d\n", global_var);
+	printk(KERN_INFO "atomic: atomic_global_var: %d\n", atomic_read(&atomic_global_var));
+	printk(KERN_INFO "spinlock: spin_global_var: %d\n", spin_global_var);
+	printk(KERN_INFO "per-cpu: percpu_global_var[0]: %d\n", per_cpu(percpu_global_var, 0));
+	printk(KERN_INFO "per-cpu: percpu_global_var[1]: %d\n", per_cpu(percpu_global_var, 1));
+	printk(KERN_INFO "per-cpu: sum: %d\n", per_cpu(percpu_global_var, 0) + per_cpu(percpu_global_var, 1));
+
+	for (i = 0; i < KTHREAD_NUM; i++)
+		printk(KERN_INFO "kthread %d run time: %llu ns\n", i, kthread_runtime_ns[i]);
 
 	printk(KERN_INFO "remove atomic module.\n");
 }
